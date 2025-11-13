15e7e981d64ac2741df2a2b6cc26c10630e79a44
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-11-14 01:31:09 +0700

    init: Spring Boot + JDK 24

---
+++ src/main/java/com/brush/butcher/ButcherApplication.java
@@ -0,0 +1,11 @@
+package com.brush.butcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ButcherApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brush/butcher/ButcherApplicationTests.java
@@ -0,0 +1,11 @@
+package com.brush.butcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcherApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
